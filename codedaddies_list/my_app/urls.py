8951520037721b9af624e5b68087888a43970cc8
siraj321 <siraj.diwan@rediffmail.com>
2020-02-25 13:26:21 +0530

    edited....

--- codedaddies_list/my_app/urls.py
+++ codedaddies_list/my_app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpattern = [
+urlpatterns = [
     path('',views.home,name='home'),
     path('admin/', admin.site.urls),
 ]
