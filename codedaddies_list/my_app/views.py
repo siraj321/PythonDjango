c85f37aff327aab04000a54e31f298bddc1eade2
siraj321 <siraj.diwan@rediffmail.com>
2020-02-26 14:53:39 +0530

    ssl verification false edited....

--- codedaddies_list/my_app/views.py
+++ codedaddies_list/my_app/views.py
@@ -1,14 +1,30 @@
-
+import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from requests import Session
+from requests.compat import quote_plus
+from . import models
 
-# Create your views here.
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search =  request.POST.get('search')
-    print(search)
+
+
+    s = requests.Session()
+    response = requests.get('https://losangeles.craigslist.org/search/bbb?query=python%20tutor',verify=False)
+    #request = requests.Request('GET', 'https://losangeles.craigslist.org/search/bbb?query=python%20tutor',verify=False)
+    #prepared_request = s.prepare_request(request)
+    #settings = s.merge_environment_settings(prepared_request.url, None, None, None, None)
+    #response = s.send(prepared_request, **settings)
+    #response = s.send(prepared_request)
+
+    data = response.text
+    print(data)
     stuff_for_frontend = {
         'search':search,
     }
